5972e59f392f12389c21857cc3f37c9f4305eb58
season.chen <563721966@qq.com>
2015-09-18 15:22:43 +0800

    'init'

---
+++ README.md
@@ -0,0 +1,4 @@
+fis-preprocessor-uri-sherry
+==
+
+> widget class 唯一
---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var path = require('path');
+
+var classes = {};
+
+module.exports = function(content, file, conf) {
+    var filepath = path.dirname(file.subPath);
+
+    if (!classes[filepath]) {
+        classes[filepath] = new Date().getTime() + '' + Math.round(Math.random(99));
+    }
+
+    if (file.isWidget) {
+        content = content.replace(/w-widget-name/ig, classes[filepath]);
+    }
+
+    return content;
+};
