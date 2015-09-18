408c421a86f40d63a68a83c8c69a6fb6b722a315
season.chen <563721966@qq.com>
2015-09-18 18:38:42 +0800

    'init'

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ var classes = {};
 module.exports = function(content, file, conf) {
     var filepath = path.dirname(file.subPath);
 
+    if (!file.isWidget) return content;
+
     if (!classes[filepath]) {
         classes[filepath] = new Date().getTime() + '' + Math.round(Math.random(99));
     }
 
-    if (file.isWidget) {
-        content = content.replace(/w-widget-class/ig, classes[filepath]);
-    }
+    content = content.replace(/w-widget-class/ig, 'w-' + classes[filepath]);
 
     return content;
 };
